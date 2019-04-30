24a62c654371c3705531bdd15d4ce8ab8bd193a1
Grant Lindsay <glindsay65@users.noreply.github.com>
2019-04-30 19:15:57 -0400

    New goals (#3)
    * Added chart with animations to Aspirations.
    
    * Improved goals pie chart to be responsive and respond to touch events.
    
    * Improved goals pie chart to be responsive and respond to touch events.

--- database/index.js
+++ database/index.js
@@ -17,9 +17,9 @@
 
 'use strict';
 
-const Cloudant = require('cloudant');
-let instance   = null;
-let contentDb  = null;
+const Cloudant = require('@cloudant/cloudant');
+let instance = null;
+let contentDb = null;
 
 /**
  * From a list of Cloudant `_id`s, fetches the documents and builds an
@@ -29,31 +29,31 @@ let contentDb  = null;
  * @returns {*}
  */
 function expandContent(ids) {
-	if (ids) {
-		return fetchDocuments(ids)
-			.then(bulk => {
-				if (bulk.rows) {
-					// Turn the fetch results into Promises that process each new doc.
-					return Promise.all(
-						// Use a reduce function to accumulate an array of only
-						// those result rows that have a document.
-						bulk.rows.reduce((acc, row) => {
-							if (row.doc) {
-								acc.push(processDocument(row.doc));
-							}
-							return acc;
-						}, []))
-						.then(newContent => {
-							// console.log('newContent:', newContent);
-							return Promise.resolve(newContent);
-						});
-				} else {
-					return Promise.resolve(null);
-				}
-			});
-	} else {
-		return Promise.resolve(null);
-	}
+  if (ids) {
+    return fetchDocuments(ids)
+      .then(bulk => {
+        if (bulk.rows) {
+          // Turn the fetch results into Promises that process each new doc.
+          return Promise.all(
+            // Use a reduce function to accumulate an array of only
+            // those result rows that have a document.
+            bulk.rows.reduce((acc, row) => {
+              if (row.doc) {
+                acc.push(processDocument(row.doc));
+              }
+              return acc;
+            }, []))
+            .then(newContent => {
+              // console.log('newContent:', newContent);
+              return Promise.resolve(newContent);
+            });
+        } else {
+          return Promise.resolve(null);
+        }
+      });
+  } else {
+    return Promise.resolve(null);
+  }
 }
 
 /**
@@ -63,21 +63,21 @@ function expandContent(ids) {
  * @returns {Promise}
  */
 function fetchDocuments(ids) {
-	return new Promise((resolve, reject) => {
-		if (contentDb) {
-			contentDb.fetch({keys: ids}, null, (err, bulk) => {
-				if (err) {
-					return reject(err);
-				}
-				resolve(bulk);
-			});
-		} else {
-			let err        = new Error('Internal Server Error');
-			err.statusCode = 500;
-			err.reason     = 'Cloudant database not opened';
-			reject(err);
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (contentDb) {
+      contentDb.fetch({ keys: ids }, null, (err, bulk) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(bulk);
+      });
+    } else {
+      let err = new Error('Internal Server Error');
+      err.statusCode = 500;
+      err.reason = 'Cloudant database not opened';
+      reject(err);
+    }
+  });
 }
 
 /**
@@ -86,18 +86,18 @@ function fetchDocuments(ids) {
  * @returns {Promise}
  */
 function getDocument(id) {
-	return new Promise((resolve, reject) => {
-		if (contentDb) {
-			contentDb.get(id, {include_docs: true}, (err, doc) => {
-				if (err) {
-					return reject(err);
-				}
-				resolve(doc);
-			});
-		} else {
-			reject(new Error('Cloudant database not opened'));
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (contentDb) {
+      contentDb.get(id, { include_docs: true }, (err, doc) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(doc);
+      });
+    } else {
+      reject(new Error('Cloudant database not opened'));
+    }
+  });
 }
 
 /**
@@ -108,52 +108,52 @@ function getDocument(id) {
  * @returns {Promise}
  */
 function processDocument(doc) {
-	return expandContent(doc.content)
-		.then(newContent => {
-			if (newContent) {
-				doc.content = newContent;
-			}
-			return Promise.resolve(doc);
-		})
-		.then(doc => {
-			// Reverse sort the skills array, when it shows up
-			if (doc.skills) {
-				doc.skills.sort((a, b) => {
-					// Descending
-					return b.level - a.level;
-				});
-			}
-			return Promise.resolve(doc);
-		});
+  return expandContent(doc.content)
+    .then(newContent => {
+      if (newContent) {
+        doc.content = newContent;
+      }
+      return Promise.resolve(doc);
+    })
+    .then(doc => {
+      // Reverse sort the skills array, when it shows up
+      if (doc.skills) {
+        doc.skills.sort((a, b) => {
+          // Descending
+          return b.level - a.level;
+        });
+      }
+      return Promise.resolve(doc);
+    });
 }
 
 module.exports = {
-	init: (appEnv, name) => {
-		// Cloudant may return a status code 429 when too many calls are made
-		// (above the threshold for the tier being used.) The built-in "retry"
-		// plugin automatically retries the calls with an exponential back-off.
-		let opts = {
-			url: appEnv.getServiceCreds(name).url,
-			plugin: 'retry',
-			retryAttempts: 5,
-			retryTimeout: 250
-		};
-		Cloudant(opts, (err, cloudant) => {
-			if (err) {
-				return console.error('Failed to initialize Cloudant', err);
-			}
-			instance = cloudant;
-			contentDb = instance.db.use('content');
-			return console.log('- Cloudant initialized');
-		});
-	},
+  init: (appEnv, name) => {
+    // Cloudant may return a status code 429 when too many calls are made
+    // (above the threshold for the tier being used.) The built-in "retry"
+    // plugin automatically retries the calls with an exponential back-off.
+    let opts = {
+      url: appEnv.getServiceCreds(name).url,
+      plugin: 'retry',
+      retryAttempts: 5,
+      retryTimeout: 250
+    };
+    Cloudant(opts, (err, cloudant) => {
+      if (err) {
+        return console.error('Failed to initialize Cloudant', err);
+      }
+      instance = cloudant;
+      contentDb = instance.db.use('content');
+      return console.log('- Cloudant initialized');
+    });
+  },
 
-	getResume: (id) => {
-		return getDocument(id)
-			.then(resume => {
-				return processDocument(resume);
-			});
-	},
+  getResume: (id) => {
+    return getDocument(id)
+      .then(resume => {
+        return processDocument(resume);
+      });
+  },
 
-	getDocument
+  getDocument
 };
